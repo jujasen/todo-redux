e4adbf28d8cb611d0815b2c67891be6410bf48cf
Julia Jakobsen <jujasen@hotmail.com>
2021-08-11 14:21:47 +0200

    added localstorage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/App";
 import { Provider } from "react-redux";
-import store from './app/store';
-import './styles/main.scss'
+import store from "./app/store";
+import "./styles/main.scss";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
